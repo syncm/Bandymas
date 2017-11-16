6a834da8336277fa0fb834cb3455f0f719db5913
syncm23 <karolis.liutac@gmail.com>
2017-11-16 17:50:05 +0200

    Changed Prideta Trikampio tikrinimo funkcionalumas

--- 5-1.php
+++ 5-1.php
@@ -8,11 +8,29 @@ $c = array("5","8","5","5");
 $i = 0;
 $d = 0;
 
+$z = 0;
 
+
+
+
+for ($z; $z <4; $z++)
+{
+$a1[$z] = $a[$z] * $a[$z];
+$b1[$z] = $b[$z] * $b[$z];
+$c1[$z] = $c[$z] * $c[$z];
+}
 
 
 for ($i; $i<4 ;$i++) 
 {
+
+if (($a1[$i] + $b1[$i] >= $c1[$i]) && ($c1[$i] + $b1[$i] >= $a1[$i]) && ($c1[$i] + $a1[$i] >= $b1[$i]))
+{
+
+
+
+
+
 if (($a[$i] == $b[$i] ) && ($a[$i] == $c[$i]))
 {
     $d = $i+1;
@@ -45,7 +63,10 @@ else if (($a[$i] !== $b[$i]) && ($a[$i] !== $c[$i]))
 echo $d,"-Ivairiakrastis, ";
 }
 
+}
+else 
 
+echo $d,"-Neimanomas trikampis ";
 
 }
 
